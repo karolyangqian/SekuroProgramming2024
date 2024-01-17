a964a84ac6e491a75e0e863102c53f186a5fde69
karolyangqian <71088489+karolyangqian@users.noreply.github.com>
2024-01-18 02:32:41 +0700

    Add files via upload

---
+++ TugasModul4/TPD_4/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/karol/TPD_4/src
+# Build directory: /home/karol/TPD_4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("package1")
